f16760c16e709b1324b2f04a4e28efcb23ef0c09
heiweilu <273925452@qq.com>
2025-04-15 21:38:20 +0800

    v0.1
    - add systolic disatolic and resistance data display
    - add PPG waveform display
    
    Signed-off-by: heiweilu <273925452@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,63 +6,76 @@
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define PPG_WIDTH 65     // PPG waveform width, in pixels
+#define PACKET_SIZE 76   // Packet size, in bytes
+#define PPG_MIN -128     // PPG waveform min value
+#define PPG_MAX 127      // PPG waveform max value
+#define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define USART_RX_BUF_SIZE 128
 uint8_t USART_RX_BUF[USART_RX_BUF_SIZE];
 
-void testdrawstyles(void) {
+static int displayCount = 0;
+
+/**
+ * @brief display waveform data and blood pressure data
+ * @note
+ *      bit 2-65: Waveform data
+ *      bit 72: Systolic blood pressure
+ * 		bit 73: Diastolic blood pressure
+ *	    bit 74: Peripheral resistance
+ */
+void displaydata() {
+  displayCount++;
   display.clearDisplay();
 
-  display.setTextSize(1);              // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);             // Start at top-left corner
-  display.println(F("Hello, world!!!"));
+  /* PPG waveform data */
+  if (displayCount >= 5) {
+    for (int i = 1; i < PPG_WIDTH; i++) {
+      int x = map(i - 1, 0, PPG_WIDTH - 1, 0, SCREEN_WIDTH / 2 - 1);
 
-  display.display();
-  delay(2000);
-}
-
-void OLED_ShowNum(int x, int y, int num, int len, uint8_t size, uint8_t color) {
-  display.setTextSize(size);
-  display.setTextColor(color);
-  display.setCursor(x, y);
-
-  // 确保数字长度符合要求
-  char str[10];
-  sprintf(str, "%0*d", len, num); // 格式化数字，确保长度为len
-  display.print(str);
-}
-
-void drawWaveform() {
-  display.clearDisplay();
-
-  // 绘制波形
-  for (int i = 1; i < 65; i++) { // 从第二个字节开始
-    int x = map(i - 1, 0, 63, 0, SCREEN_WIDTH - 1); // 映射x坐标
-
-    // 将有符号数转换为无符号数
-    int value = (int8_t)USART_RX_BUF[i];
-    int y = map(value, -128, 127, SCREEN_HEIGHT - 1, 0); // 映射y坐标
-
-    // 绘制点
-    display.drawPixel(x, y, SSD1306_WHITE);
+      int value = (int8_t)USART_RX_BUF[i];
+      int y = map(value, PPG_MIN, PPG_MAX, SCREEN_HEIGHT - 1, 0);
+      display.drawPixel(x, y, SSD1306_WHITE);
+    }
+    displayCount = 0;
   }
 
-  // 更新显示
+  /* Blood pressure data */
+  int systolic = USART_RX_BUF[71];
+  int diastolic = USART_RX_BUF[72];
+  int resistance = USART_RX_BUF[73];
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
+  display.setCursor(SCREEN_WIDTH / 2, 0);
+  display.print("Sys: ");
+  display.print(systolic);
+
+  display.setCursor(SCREEN_WIDTH / 2, 10);
+  display.print("Dia: ");
+  display.print(diastolic);
+
+  display.setCursor(SCREEN_WIDTH / 2, 20);
+  display.print("Res: ");
+  display.print(resistance);
+
   display.display();
 }
 
 void setup() {
-  Serial.begin(38400); // 更改为常用的115200波特率
+
+  /* Initialize serial; MKS-141 :38400; Send 0x8a to start*/
+  Serial.begin(38400);
   Serial.write(0x8a);
 
-  // 初始化OLED显示
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // 0x3C是OLED的I2C地址
+  /* Initialize OLED*/
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
+    for (;;)
+      ;
   }
   display.clearDisplay();
   display.display();
@@ -74,17 +87,16 @@ void loop() {
   if (Serial.available() > 0) {
     int incomingByte = Serial.read();
 
+    /* Frame Header: 0xFF*/
     if (dataCount == 0 && incomingByte != 0xFF) {
-      // 如果第一个字节不是 0xFF，丢弃该字节
       return;
     }
 
-    if (dataCount < 65) {
+    if (dataCount < PACKET_SIZE) {
       USART_RX_BUF[dataCount++] = incomingByte;
 
-      if (dataCount == 65) {
-        // 接收到完整数据，处理数据
-        drawWaveform();
+      if (dataCount == PACKET_SIZE) {
+        displaydata();
         dataCount = 0;
         memset(USART_RX_BUF, 0, sizeof(USART_RX_BUF));
       }
